152a45aebaa1bde92c8f64accef661f6b860770b
chugn <chungteocu@gmail.com>
2026-08-25 20:17:55 +0700

    Create DSA08002 — CẤU TRÚC DỮ LIỆU HÀNG ĐỢI 2.cpp

---
+++ DSA08002 — CẤU TRÚC DỮ LIỆU HÀNG ĐỢI 2.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    queue<int> q;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        if (s == "PUSH")
+        {
+            int x;
+            cin >> x;
+            q.push(x);
+        }
+        else if (s == "PRINTFRONT")
+        {
+            if (!q.empty())
+                cout << q.front() << "\n";
+            else
+                cout << "NONE\n";
+        }
+        else if (s == "POP")
+        {
+            if (!q.empty())
+                q.pop();
+        }
+    }
+}
