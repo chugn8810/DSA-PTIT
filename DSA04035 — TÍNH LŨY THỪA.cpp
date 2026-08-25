f7f617939be9c4c0ad0576d4397ce2ac32e5744c
chugn <chungteocu@gmail.com>
2026-08-25 20:18:46 +0700

    Create  DSA04035 — TÍNH LŨY THỪA.cpp

---
+++ DSA04035 — TÍNH LŨY THỪA.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MOD = 1e9 + 7;
+int main()
+{
+    while (1)
+    {
+        long long a, b;
+        cin >> a >> b;
+        if (a == 0 && b == 0)
+            break;
+        long long res = 1;
+        a % MOD;
+        while (b > 0)
+        {
+            if (b % 2 == 0)
+            {
+                a = (a % MOD * a % MOD) % MOD;
+                b /= 2;
+            }
+            else if (b % 2 != 0)
+            {
+                res = (a % MOD * res % MOD) % MOD;
+                b -= 1;
+            }
+        }
+        cout << res << endl;
+    }
+}
