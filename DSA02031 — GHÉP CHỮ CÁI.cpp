7783dbcfa125226692b891777c094ef454008155
chugn <chungteocu@gmail.com>
2026-09-05 22:30:39 +0700

    Create DSA02031 — GHÉP CHỮ CÁI.cpp

---
+++ DSA02031 — GHÉP CHỮ CÁI.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+void sinh(int m)
+{
+    int dem = 1;
+    vector<int> a(m);
+    iota(a.begin(), a.end(), 1);
+    int test = 0;
+    for (int j = 1; j < m - 1; j++)
+    {
+        if (a[j] == 1 || a[j] == 5)
+        {
+            test = 1;
+            break;
+        }
+    }
+    if (test == 0)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            char tmp = a[j] + 64;
+            cout << tmp;
+        }
+        cout << endl;
+    }
+    int i = m - 2;
+    while (i > -1)
+    {
+        i = m - 2;
+        while (i >= 0 && a[i] > a[i + 1])
+        {
+            i--;
+        }
+
+        if (i == -1)
+            break;
+        int vitri = -1;
+        for (int j = i + 1; j < m; j++)
+        {
+            int min = 100;
+            if (a[j] > a[i] && min > a[j])
+            {
+                min = a[j];
+                vitri = j;
+            }
+        }
+        swap(a[i], a[vitri]);
+        reverse(a.begin() + i + 1, a.end());
+        int check = 0;
+        for (int j = 0; j < m - 1; j++)
+        {
+            if ((a[j] == 1 && a[j + 1] == 5) || (a[j] == 5 && a[j + 1] == 1))
+            {
+                check = 1;
+                break;
+            }
+        }
+        if (check == 1)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                char tmp = a[j] + 64;
+                cout << tmp;
+            }
+            cout << endl;
+            continue;
+        }
+        for (int j = 1; j < m - 1; j++)
+        {
+            if (a[j] == 1 || a[j] == 5)
+            {
+                check = 1;
+                break;
+            }
+        }
+        if (check == 1)
+            continue;
+        for (int j = 0; j < m; j++)
+        {
+            char tmp = a[j] + 64;
+            cout << tmp;
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    char m;
+    cin >> m;
+    int n = m - 64;
+    sinh(n);
+}
