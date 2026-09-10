7a49b2321d44b4f3a4945a9d7e7b36526b539c31
chugn <chungteocu@gmail.com>
2026-09-10 22:11:29 +0700

    Create  DSA03008 — SẮP XẾP CÔNG VIỆC 1.cpp

---
+++ DSA03008 — SẮP XẾP CÔNG VIỆC 1.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool sosanh(pair<int, int> a, pair<int, int> b)
+{
+    if (a.second == b.second)
+    {
+        return a.first > b.first;
+    }
+    return a.second < b.second;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int m;
+        cin >> m;
+        vector<pair<int, int>> a(m);
+        for (int i = 0; i < m; i++)
+        {
+            cin >> a[i].first;
+        }
+        for (int i = 0; i < m; i++)
+        {
+            cin >> a[i].second;
+        }
+        sort(a.begin(), a.end(), sosanh);
+        int start = a[0].second, res = 1;
+        for (int i = 1; i < m; i++)
+        {
+            if (a[i].first >= start)
+            {
+                res++;
+                start = a[i].second;
+            }
+        }
+        cout << res << "\n";
+    }
+}
