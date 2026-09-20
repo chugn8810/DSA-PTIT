7ef39f5e06ac48a01b6c8556b425caf54bf4fed2
chugn <chungteocu@gmail.com>
2026-09-20 16:59:47 +0700

    Create DSA09012 — LIỆT KÊ ĐỈNH TRỤ.cpp

---
+++ DSA09012 — LIỆT KÊ ĐỈNH TRỤ.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool used[1001];
+void DFS(int a, vector<int> adj[1001])
+{
+    queue<int> q;
+    q.push(a);
+    while (!q.empty())
+    {
+        int x = q.front();
+        used[x] = true;
+        q.pop();
+        for (auto it : adj[x])
+        {
+            if (!used[it])
+                q.push(it);
+        }
+    }
+}
+int main()
+{
+    int m;
+    cin >> m;
+    while (m--)
+    {
+        vector<int> adj[1001];
+        memset(used, false, sizeof(used));
+        int m, n;
+        cin >> m >> n;
+        for (int i = 0; i < n; i++)
+        {
+            int x, y;
+            cin >> x >> y;
+            adj[x].push_back(y);
+            adj[y].push_back(x);
+        }
+        int tong = 0;
+        for (int i = 1; i <= m; i++)
+        {
+            if (!used[i])
+            {
+                DFS(i, adj);
+                tong++;
+            }
+        }
+        vector<int> res;
+        for (int i = 1; i <= m; i++)
+        {
+            memset(used, false, sizeof(used));
+            used[i] = true;
+            int tmp = 0;
+            for (int i = 1; i <= m; i++)
+            {
+                if (!used[i])
+                {
+                    DFS(i, adj);
+                    tmp++;
+                }
+            }
+            if (tmp > tong)
+            {
+                res.push_back(i);
+            }
+        }
+        if (!res.empty())
+            for (int i = 0; i < res.size(); i++)
+            {
+                cout << res[i] << " ";
+            }
+        else
+            cout << 0;
+        cout << endl;
+    }
+}
